11372053b0e15b4d56070cba4102b6b694d5e26a
AdrienFerrer <adrien.ferrer@isep.fr>
2018-12-10 00:23:21 +0100

    delete

--- js/index.js
+++ js/index.js
@@ -2,29 +2,37 @@
 
 var id=0;
 
-var units = [];
+
+var tableau = document.getElementById("tableau");
+
 
 function create_unit(){
-	var unit=[];
 	id+=1;
-	//console.log(document.getElementById('unit_type').value);
-	//console.log(id);
-	//console.log(document.getElementById('unit_name').value);
-	unit = [document.getElementById('unit_type').value,id,document.getElementById('unit_name').value];
-	units.push(unit);
-		var tableau = document.getElementById("tableau");
-	
-		var ligne = tableau.insertRow(-1);//on a ajouté une ligne
-	
-		var colonne1 = ligne.insertCell(0);//on a une ajouté une cellule
-		colonne1.innerHTML += document.getElementById("unit_type").value;//on y met le contenu de titre
+		
 
-		var colonne2 = ligne.insertCell(1);//on ajoute la seconde cellule
-		colonne2.innerHTML += "#"+id;
 	
-		var colonne3 = ligne.insertCell(2);//on ajoute la seconde cellule
-		colonne3.innerHTML += document.getElementById("unit_name").value;
+	var ligne = tableau.insertRow(-1);//on a ajouté une ligne
+	
+	var colonne1 = ligne.insertCell(0);//on a une ajouté une cellule
+	colonne1.innerHTML += document.getElementById("unit_type").value;//on y met le contenu de titre
+
+	var colonne2 = ligne.insertCell(1);//on ajoute la seconde cellule
+	colonne2.innerHTML += "#"+id;
+	
+	var colonne3 = ligne.insertCell(2);//on ajoute la seconde cellule
+	colonne3.innerHTML += document.getElementById("unit_name").value;
+
+	var colonne4 = ligne.insertCell(3);//on ajoute la seconde cellule
+	colonne3.innerHTML += '<button onclick="delete_unit("'+id+'")">Delete</button>' ;
 	
 }
+
+function delete_unit(id){
+	alert(id);
+	tableau.deleteRow(id);
+
+}
+
+
 
 
