68b74144bf3474ca900558173c196b612fc264cb
Pierre Goffic <pierre.goffic@isep.fr>
2018-12-09 23:59:31 +0100

    Tableau

--- js/index.js
+++ js/index.js
@@ -12,7 +12,16 @@ function create_unit(){
 	//console.log(document.getElementById('unit_name').value);
 	unit = [document.getElementById('unit_type').value,id,document.getElementById('unit_name').value];
 	units.push(unit);
-
+		var tableau = document.getElementById("tableau");
+	
+		var ligne = tableau.insertRow(-1);//on a ajouté une ligne
+	
+		var colonne1 = ligne.insertCell(0);//on a une ajouté une cellule
+		colonne1.innerHTML += document.getElementById("unit_type").value;//on y met le contenu de titre
+	
+		var colonne2 = ligne.insertCell(1);//on ajoute la seconde cellule
+		colonne2.innerHTML += document.getElementById("unit_name").value;
+	
 }
 
 
